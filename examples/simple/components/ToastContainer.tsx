be01c35e6c72957e224f7ce64be23285a763660b
Gino <admin@gino.dev>
2022-08-22 14:24:11 +0200

    Added usage of the useToastTimer to the simple example, to show how you can make toasts disappear after a few seconds automatically

--- examples/simple/components/ToastContainer.tsx
+++ examples/simple/components/ToastContainer.tsx
@@ -1,4 +1,4 @@
-import { useToast } from "@gino/toast";
+import { Toast as IToast, useToast, useToastTimer } from "@gino/toast";
 
 export function ToastContainer() {
   const { toasts, removeToast } = useToast();
@@ -6,22 +6,35 @@ export function ToastContainer() {
   return (
     <ul className="absolute bottom-8 right-8 flex flex-col gap-4 max-w-md w-full">
       {toasts.map((toast) => (
-        <li
-          key={toast.id}
-          className="bg-blue-600 text-white px-4 py-3 rounded-md flex items-center"
-        >
-          <span className="flex-1">
-            {toast.message} ({toast.id})
-          </span>
-
-          <button
-            className="bg-black/10 px-4 py-2 text-sm font-semibold rounded"
-            onClick={() => removeToast(toast.id)}
-          >
-            Dismiss
-          </button>
-        </li>
+        <Toast key={toast.id} toast={toast} />
       ))}
     </ul>
+  );
+}
+
+function Toast({ toast }: { toast: IToast }) {
+  const { removeToast } = useToast();
+
+  const ref = useToastTimer<HTMLLIElement>(toast.id, {
+    duration: 3, // In seconds,
+    pauseOnHover: true,
+  });
+
+  return (
+    <li
+      ref={ref}
+      className="bg-blue-600 text-white px-4 py-3 rounded-md flex items-center"
+    >
+      <span className="flex-1">
+        {toast.message} ({toast.id})
+      </span>
+
+      <button
+        className="bg-black/10 px-4 py-2 text-sm font-semibold rounded"
+        onClick={() => removeToast(toast.id)}
+      >
+        Dismiss
+      </button>
+    </li>
   );
 }
